f1536f58021971bd29dbea1667f53bf4495b2700
kuki <goudkanishka@gmail.com>
2024-08-06 10:43:57 +0530

    update

--- problem_2.js
+++ problem_2.js
@@ -1,24 +1,22 @@
 // The Double Trouble: You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
 
-function arrayElement(element){
+function arrayElement(array){
+    let lastElement=null;
 
-    arrDouble.push(element*2)
+        for(let i=0; i<array.length; i++){
+            let currentElement= arr[i];
+
+            if (currentElement != lastElement){
+                arrDouble.push(currentElement*currentElement)
+                lastElement=currentElement;
+            }
+       }
+       return arrDouble;
 }
 
 let arr=[88,43,6,2, 2, 54,53]
 let arrDouble=[]
 
-for (const element of arr) {
-    for(let i=0; i<arr.length; i++){
-        for (let j=1; j<arr.length; j++){         
-            try {
-                arr[i]==arr[j]
-            } catch (error) {
-                continue;
-            }
-        }
-        arrayElement(arr[i])
-    }
-}
-console.log(arrDouble)
+let result= arrayElement(arr)
+console.log(result)
 // arr=[176 , 86 ,12, 4,108,106 ]
