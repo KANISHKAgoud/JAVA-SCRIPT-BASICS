025670f0ca36631da4f43a9afd044eda1bb88103
kuki <goudkanishka@gmail.com>
2024-08-07 12:41:43 +0530

    changes

--- hacking-simulator/script.js
+++ hacking-simulator/script.js
@@ -16,12 +16,28 @@ const delay = () =>{
     })   
 }
 
-let arr =[ "Initializing Hacking", 
-    "Reading your Files", 
-    "Password files detected", 
-    "Sending all passwords and personal files to server", 
-    "Cleaning up"]
+async function main() {
+    
+    let dotLoop=() => {
+        setInterval(() => {
+            if ()
+            let dots=document.createElement("div")
+            console.log(".")
+            div.append(dots)
+        }, 700);
 
-for (const element of arr) {
-    Hacking(element)
-}
+    }
+    
+    
+    let arr =[ "Initializing Hacking", 
+        "Reading your Files", 
+        "Password files detected", 
+        "Sending all passwords and personal files to server", 
+        "Cleaning up"]
+        
+        for (const element of arr) {
+            Hacking(element)
+        }
+    }
+
+main()
