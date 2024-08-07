da86ce2b3bc1354689a0e97f44a5767f3d2389c8
kuki <goudkanishka@gmail.com>
2024-08-07 19:30:40 +0530

    update

--- problems/program_7/problem_7.js
+++ problems/program_7/problem_7.js
@@ -1,9 +1,8 @@
 // The Local Storage Manager: You are working on a note-taking app, and you want to implement a function named saveNoteToLocalStorage that takes a note object and saves it to the browser's local storage
 
 function saveNoteToLocalStorage(note){
-    localStorage.setItem(note, note)
-    console.log(localStorage.getItem(note))
-
+    localStorage.setItem("note", JSON.stringify(note))
+    JSON.parse(localStorage.getItem("note"))
 }
 
 const note ={
